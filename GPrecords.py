1ffb3bd92b82c8c53579a27b93c4088d57ebc134
Sebastian Bitzer <official@sbitzer.eu>
2018-05-06 15:10:24 +0200

    first seemingly working run with GP model
    The interence result uses a GP with huge lengthscale such that GP latent
    variables all have the same (wide) distribution. So the GP explains nothing and
    all variance is explained by variability in athlete abilities. This is
    surprising, because the prior over rho was relatively narrow with 90th
    percentile at 24.31.

--- GPrecords.py
+++ GPrecords.py
@@ -39,32 +39,62 @@ data {
     
     int<lower=1> athletes[N, AY];
     vector[N] records;
-    vector[N] years;
+    real years[N];
     int<lower=0> recordsa[N];
     
     real<lower=0> astd_std;
 }
 
+transformed data {
+    real delta = 1e-9;
+}
+
 parameters {
     real worldspeed;
+        
+    real<lower=0> rho_raw;
+    real<lower=0> alpha;
+    vector[N] eta;
+
     real<lower=0> ability_std;
     vector[A] abilities;
     vector<lower=0>[A] inconsistencies;
 }
 
 transformed parameters {
-    vector[A] ameans = worldspeed + 10 + abilities * ability_std * 2;
+    real rho = exp(rho_raw * 2 + 10);
+    vector[A] ameans = abilities * ability_std * 2;
     vector[A] astds = inconsistencies * astd_std;
+    vector[N] f;
+    {
+        matrix[N, N] L_K;
+        matrix[N, N] K = cov_exp_quad(years, alpha, rho);
+        // diagonal elements
+        for (n in 1:N)
+            K[n, n] = K[n, n] + delta;
+        L_K = cholesky_decompose(K);
+        f = L_K * eta;
+    }
 }
 
 model {
+    eta ~ normal(0, 1);
+    alpha ~ normal(0, 1);
+    
+    // effective prior for rho: lognormal(log(10), log(2))
+    rho_raw ~ normal(0, 1);
+
+    // effective prior: normal(10, 1)
     worldspeed ~ normal(0, 1);
+    
     ability_std ~ normal(0, 1);
     abilities ~ normal(0, 1);
     inconsistencies ~ normal(0, 1);
     
-    for (n in 1:N)
-        records[n] ~ GaussRecord(recordsa[n], athletes[n], ameans, astds);
+    for (n in 1:N) {
+        records[n] ~ GaussRecord(recordsa[n], athletes[n], 
+                                 f[n] + worldspeed + 10 + ameans, astds);
+    }
 }"""
 
 sm = pystan.StanModel(model_code=modelcode)
